770d39fa4d645bf7b0841d074541de61acea3ade
Vik Paruchuri <vik@edx.org>
2013-03-18 10:15:35 -0400

    Start on getting started docs

---
+++ docs/examples/connect_to_api.py
@@ -0,0 +1,28 @@
+import requests
+
+API_BASE_URL = "http://127.0.0.1:7999"
+
+#This queries the top level schema and gets all of the available models, and their associated endpoints and schema.
+#The ?format=json is needed to let the API know how to return the data.
+#Supported formats are 'json', 'jsonp', 'xml', 'yaml', 'plist'.
+response = requests.get(API_BASE_URL + "/essay_site/api/v1/?format=json")
+
+#This status code should be 200.  If it is not, verify that you are running the api server at the API_BASE_URL
+#You can use python manage.py runserver 127.0.0.1:7999 to accomplish this.
+print("Status Code: {0}".format(response.status_code))
+
+#Decode the json serialized response into a python object.
+rj = response.json()
+print(rj)
+
+#Loop through the json object and print out the data.
+for model in rj:
+    print("Model: {0} Endpoint: {1} Schema: {2}".format(model, rj[model]['list_endpoint'], rj[model]['schema']))
+
+#Now, let's try to get the schema for a single model.
+response = requests.get(API_BASE_URL + rj['essay']['schema'] + "?format=json")
+
+#This should get a 401 error because you are not logged in.
+print("Status Code: {0}".format(response.status_code))
+
+
---
+++ docs/examples/create_user.py
@@ -0,0 +1,5 @@
+import requests
+
+API_BASE_URL = "http://127.0.0.1:7999"
+
+
