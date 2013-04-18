6d7403e0c4fe8aaf3f51c4aeddc7aee3489562f5
Vik Paruchuri <vik@edx.org>
2013-04-18 15:09:54 -0400

    More tests, start working on object creation

--- freeform_data/tests.py
+++ freeform_data/tests.py
@@ -37,13 +37,27 @@ def get_urls(resource_name):
     schema = reverse("api_get_schema", kwargs={'api_name': 'v1','resource_name': resource_name})
     return endpoint,schema
 
+def get_first_resource_uri(type):
+    c = login()
+    endpoint, schema = get_urls(type)
+    data = c.get(endpoint, data={'format' : 'json'})
+    log.debug(data.content)
+    object = json.loads(data.content)['objects'][0]
+    resource_uri = [object['resource_uri']]
+    return resource_uri
+
+def login():
+    c = Client()
+    c.login(username='test', password='test')
+    return c
+
 class GenericTest(object):
     type = "generic"
+    object = {'hello' : 'world'}
 
     def generic_setup(self):
         run_setup()
-        self.c = Client()
-        self.c.login(username='test', password='test')
+        self.c = login()
         self.endpoint, self.schema = get_urls(self.type)
 
     def test_schema(self):
@@ -60,26 +74,44 @@ class GenericTest(object):
 
         self.assertEqual(result.status_code,200)
 
+    def test_create(self):
+        result = self.c.post(self.endpoint, json.dumps(self.object), "application/json")
+        self.assertEqual(result.status_code,201)
+
 
 class OrganizationTest(unittest.TestCase, GenericTest):
     type="organization"
+    object = {"name" : "edX"}
+
     def setUp(self):
         self.generic_setup()
 
 class CourseTest(unittest.TestCase, GenericTest):
     type="course"
+    object = {'course_name' : "edx_test"}
     def setUp(self):
         self.generic_setup()
 
 class ProblemTest(unittest.TestCase, GenericTest):
     type="problem"
+
     def setUp(self):
         self.generic_setup()
+        self.create_object()
+
+    def create_object(self):
+        course_resource_uri = get_first_resource_uri("course")
+        self.object = {'courses' : course_resource_uri}
 
 class EssayTest(unittest.TestCase, GenericTest):
     type="essay"
     def setUp(self):
         self.generic_setup()
+        self.create_object()
+
+    def create_object(self):
+        problem_resource_uri = get_first_resource_uri("problem")
+        self.object = {'problem' : problem_resource_uri}
 
 class EssayGradeTest(unittest.TestCase, GenericTest):
     type="essaygrade"
