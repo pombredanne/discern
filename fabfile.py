6e0ef4963c285b4553b20f93a2f0d03a7cc4d100
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-04-26 00:02:02 -0400

    Fabfile cleanup

--- fabfile.py
+++ fabfile.py
@@ -15,14 +15,8 @@ import sys
 import os
 import logging
 
-#Should be in the format user@remote_host
-#env.hosts = ['vik@sandbox-service-api-001.m.edx.org']
-
 # Deploy to Vagrant with:
 # fab  -i /Users/nateaune/.rvm/gems/ruby-1.9.3-p374/gems/vagrant-1.0.7/keys/vagrant deploy
-
-# TODO: make this configurable
-env.hosts = ['vagrant@33.33.33.10']
 
 # Usage:
 # MacOSX: 
@@ -32,35 +26,49 @@ env.hosts = ['vagrant@33.33.33.10']
 # Debian/Ubuntu: 
 # fab -i /opt/vagrant/embedded/gems/gems/vagrant-1.0.3/keys/vagrant deploy
 
-
+#Define this path so that we can import the django settings
 ROOT_PATH = path(__file__).dirname()
-
 sys.path.append(ROOT_PATH)
 
+#Use django settings to define some paths
 django.settings_module('ml_service_api.settings')
 from django.conf import settings as django_settings
 
+#Disable annoyting log messages.
 logging.basicConfig(level=logging.ERROR)
 
-# remote_user = 'vik'
-remote_user = 'vagrant'
+#User settings.
+
+# Set local_user to the name of your current user on your local machine
 # local_user = 'vik'
 local_user = 'nateaune'
 
+#Remote user is the user you want to use once you ssh into the box specified at env.hosts.
+# remote_user = 'vik'
+remote_user = 'vagrant'
+
+#This makes the paramiko logger less verbose
 para_log=logging.getLogger('paramiko.transport')
 para_log.setLevel(logging.ERROR)
 
+#Use the below setting to pick the remote host that you want to deploy to.
+# TODO: make this configurable
 #Should be in the format user@remote_host
 env.hosts = ['vik@sandbox-service-api-001.m.edx.org']
-#Can set this to be a path to a local keyfile if nonstandard
-#env.key_filename = ''
+#env.hosts = ['vagrant@33.33.33.10']
 
 def prepare_deployment():
+    """
+    Make a commit and push it to github
+    """
     #Make a local commit with latest changes if needed.
     local('git add -p && git commit')
     local("git push")
 
 def check_paths():
+    """
+    Ensure that the paths are correct.
+    """
     log = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO)
     log.info(django_settings.ROOT_PATH)
@@ -68,15 +76,19 @@ def check_paths():
     log.info(django_settings.ENV_ROOT)
 
 def deploy():
+    """
+    Deploy to a server.
+    """
+
+    #Forwards your local key to the remote machine
     env.forward_agent = True
+
     #Setup needed directory paths
-    #Edit if you are using this for deployment
+    #May need to edit if you are using this for deployment
     up_one_level_dir = '/opt/wwc'
     code_dir = os.path.join(up_one_level_dir, "ml-service-api")
     ml_code_dir = os.path.join(up_one_level_dir, 'machine-learning')
     database_dir = os.path.join(code_dir, "db")
-    remote_ssh_dir = '/home/vik/.ssh'
-    local_dir = django_settings.ENV_ROOT
     nltk_data_dir = '/usr/share/nltk_data'
     static_dir = os.path.join(code_dir, 'staticfiles')
     deployment_config_dir = os.path.join(django_settings.REPO_PATH, "deployment/configuration/")
@@ -139,22 +151,13 @@ def deploy():
             sudo('mkdir -p /opt/edx')
             sudo('virtualenv "/opt/edx"')
             # TODO: should not be hardcoded to vik
-            sudo('chown -R {0} /opt/edx'.remote_user)
+            sudo('chown -R {0} /opt/edx'.format(remote_user))
 
     with prefix('source /opt/edx/bin/activate'), settings(warn_only=True):
-        #sudo('apt-get build-dep python-scipy')
-        #Numpy and scipy are a bit special in terms of how they install
-        run('pip install numpy==1.6.2')
-        #This is needed to support the ml algorithm
-        sudo('mkdir -p {0}'.format(nltk_data_dir))
-        if not exists(nltk_data_dir):
-            sudo('python -m nltk.downloader -d {0} all'.format(nltk_data_dir))
-        # TODO: don't hardcode vik
-        sudo('chown -R {0} {1}'.format(remote_user, nltk_data_dir))
-
         with cd(code_dir):
+            #Numpy and scipy are a bit special in terms of how they install, so we need pre-requirements.
+            run('pip install -r pre-requirements.txt')
             run('pip install -r requirements.txt')
-
             # Sync django db and migrate it using south migrations
             run('python manage.py syncdb --noinput --settings=ml_service_api.aws --pythonpath={0}'.format(code_dir))
             run('python manage.py migrate --settings=ml_service_api.aws --pythonpath={0}'.format(code_dir))
@@ -167,10 +170,17 @@ def deploy():
             sudo('xargs -a apt-packages.txt apt-get install')
             run('pip install -r pre-requirements.txt')
             run('pip install -r requirements.txt')
+
+            #This is needed to support the ml algorithm
+            sudo('mkdir -p {0}'.format(nltk_data_dir))
+            if not exists(nltk_data_dir):
+                sudo('python -m nltk.downloader -d {0} all'.format(nltk_data_dir))
+                # TODO: don't hardcode vik
+            sudo('chown -R {0} {1}'.format(remote_user, nltk_data_dir))
             run('python setup.py install')
 
     with lcd(deployment_config_dir), settings(warn_only=True):
-		sudo('mkdir -p /etc/nginx/sites-available')
+        sudo('mkdir -p /etc/nginx/sites-available')
         with cd(up_one_level_dir):
             #Move env and auth.json (read by aws.py if using it instead of settings)
             put('service-auth.json', 'auth.json', use_sudo=True)
@@ -183,7 +193,8 @@ def deploy():
             #Modify nginx settings to pass through ml-service-api
             put('service-nginx', 'default', use_sudo=True)
 
-    #Start all services back up
-    sudo('service celery start')
-    sudo('service ml-service-api start')
-    sudo('service nginx restart')
+    with settings(warn_only=True):
+        #Start all services back up
+        sudo('service celery start')
+        sudo('service ml-service-api start')
+        sudo('service nginx restart')
