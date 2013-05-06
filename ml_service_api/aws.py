c27d51e62177f074b9bb2783a824bb8d592f74f0
Vik Paruchuri <vik.paruchuri@gmail.com>
2013-05-06 15:37:15 -0400

    change paths to auth and env json files

--- ml_service_api/aws.py
+++ ml_service_api/aws.py
@@ -16,10 +16,10 @@ THROTTLE_AT = 100 #Throttle requests after this number in below timeframe
 THROTTLE_TIMEFRAME= 60 * 60 #Timeframe in which to throttle N requests, seconds
 THROTTLE_EXPIRATION= 24 * 60 * 60 # When to remove throttle entries from cache, seconds
 
-with open(ENV_ROOT + "env.json") as env_file:
+with open(os.path.join(ENV_ROOT,"env.json")) as env_file:
     ENV_TOKENS = json.load(env_file)
 
-with open(ENV_ROOT + "auth.json") as auth_file:
+with open(os.path.join(ENV_ROOT, "auth.json")) as auth_file:
     AUTH_TOKENS = json.load(auth_file)
 
 DATABASES = AUTH_TOKENS.get('DATABASES', DATABASES)
